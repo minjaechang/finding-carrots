43d1da8f01a0f888fc509c11a70ba29cdcb5387f
Minjae Chang <minjaejchang@gmail.com>
2022-02-27 17:35:57 +1300

    Implement addItem function

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const CARROT_SIZE = 80;
+
+const gameButton = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+const gameField = document.querySelector('.game__field');
+const fieldRect = gameField.getBoundingClientRect();
+
+function initGame() {
+  // locate bugs and carrots randomly in places
+  addItem('carrot', 5, 'img/carrot.png');
+  addItem('bug', 5, 'img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+
+    item.style.position = 'absolute';
+
+    const x = Math.random() * (x2 - x1) + x1;
+    const y = Math.random() * (y2 - y1) + y1;
+
+    item.style.left = x + 'px';
+    item.style.top = y + 'px';
+
+    gameField.append(item);
+  }
+}
+
+initGame();
